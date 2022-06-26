6f9ba53b939c32412eae1b6c382b90342f8fa441
MahmudK <mahmudkenway@gmail.com>
2022-06-26 18:08:05 +0430

    add login page

---
+++ pages/auth/login.js
@@ -0,0 +1,7 @@
+import Header from "../../components/layout/header";
+import Login from "../../components/auth/login";
+export default function LoginPage() {
+    return(
+        <Login />
+    )
+}
