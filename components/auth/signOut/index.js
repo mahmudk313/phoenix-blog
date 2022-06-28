51a392313d2ad334377ad56b980c7a35b15461ac
MahmudK <mahmudkenway@gmail.com>
2022-06-28 10:18:29 +0430

    Separate SignOut to SignOutTitle & SignOutForm

--- components/auth/signOut/index.js
+++ components/auth/signOut/index.js
@@ -1,4 +1,6 @@
-import country from "../../general/country";
+//Components
+import SignOutTitle from "./signOutTitle";
+import SignOutForm from "./signOutForm";
 
 export default function SignOut() {
     
@@ -6,119 +8,8 @@ export default function SignOut() {
         <>
             <div className="mt-10 sm:mt-0 h-full bg-gray-50">
                 <div className="min-h-full flex flex-col items-center justify-center p-4 sm:px-6 lg:px-8">
-                    <div className="py-4">
-                        <div className="px-4 sm:px-0">
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">Create a new account</h3>
-                            <p className="mt-1 text-sm text-gray-600"></p>
-                        </div>
-                    </div>
-                    <div className="mt-5 md:mt-0">
-                        <form action="#" method="POST">
-                            <div className="shadow overflow-hidden sm:rounded-md">
-                                <div className="px-4 py-5 bg-white sm:p-6">
-                                    <div className="grid grid-cols-6 gap-6">
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                                                First name
-                                            </label>
-                                            <input
-                                                type="text"
-                                                name="first-name"
-                                                id="first-name"
-                                                required
-                                                autoComplete="given-name"
-                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                            />
-                                        </div>
-
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                                                Last name
-                                            </label>
-                                            <input
-                                                type="text"
-                                                name="last-name"
-                                                id="last-name"
-                                                required
-                                                autoComplete="family-name"
-                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                            />
-                                        </div>
-
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                                                Email address
-                                            </label>
-                                            <input
-                                                type="email"
-                                                name="email-address"
-                                                id="email-address"
-                                                required
-                                                autoComplete="email"
-                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                            />
-                                        </div>
-
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                                                Country
-                                            </label>
-                                            <select
-                                                id="country"
-                                                name="country"
-                                                autoComplete="country-name"
-                                                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                            >
-                                                {
-                                                    country.map(item => <option value={item.value}>{item.text}</option>)
-                                                }
-                                            </select>
-                                        </div>
-
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                                                Password
-                                            </label>
-                                            <input
-                                                type="password"
-                                                name="password"
-                                                id="password"
-                                                required
-                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                            />
-                                        </div>
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                                                Confirm Password
-                                            </label>
-                                            <input
-                                                type="password"
-                                                name="confirmPassword"
-                                                id="confirmPassword"
-                                                required
-                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                            />
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                     <button
-                                        type="submit"
-                                        className="mr-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm text-sm font-medium rounded-md text-indigo-700 hover:text-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        type="submit"
-                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                    <SignOutTitle />
+                    <SignOutForm />
                 </div>
             </div>
             
---
+++ components/auth/signOut/signOutTitle.js
@@ -0,0 +1,10 @@
+export default function SignOutTitle() {
+    return(
+        <div className="py-4">
+            <div className="px-4 sm:px-0">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">Create a new account</h3>
+                <p className="mt-1 text-sm text-gray-600"></p>
+            </div>
+        </div>
+    )
+}
