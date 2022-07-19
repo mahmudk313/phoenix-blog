936356e8aa2e67322ca47c26f80a1fced03096c4
MahmudK <mahmudkenway@gmail.com>
2022-07-19 18:32:24 +0430

    Create filterForm component in users comp

--- components/adminPanel/users/index.js
+++ components/adminPanel/users/index.js
@@ -1,5 +1,6 @@
 //Components
 import UsersTable from "./usersTable"
+import FilterForm from "./filterForm"
 
 export default function Users() {
 
@@ -7,22 +8,10 @@ export default function Users() {
         <main>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <div className="container mx-auto px-4 sm:px-8 max-w-7xl">
-                    <div className="py-8">
-                        <div className="flex flex-row mb-1 sm:mb-0 justify-between w-full">
-                            <h2 className="text-2xl leading-tight">
-                                Users
-                            </h2>
-                            <div className="text-end">
-                                <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
-                                    <div className=" relative ">
-                                        <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name"/>
-                                    </div>
-                                    <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                                        Filter
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
+                    <div className="py-4">
+                        
+                        <FilterForm />
+
                         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
                                 
