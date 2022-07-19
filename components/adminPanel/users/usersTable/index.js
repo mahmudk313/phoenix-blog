6d280aa3075f571cd7b89081a5cbecaa44218c50
MahmudK <mahmudkenway@gmail.com>
2022-07-19 20:36:58 +0430

    Receive users from store defined

--- components/adminPanel/users/usersTable/index.js
+++ components/adminPanel/users/usersTable/index.js
@@ -3,7 +3,12 @@ import TableHead from "./tableHead"
 import UsersTableRow from "./usersTableRow"
 import Pagination from "./pagination"
 
+//redux-store
+import { useSelector } from "react-redux"
+
 export default function UsersTable () {
+    const users = useSelector(state => state.users.list)
+    console.log(users)
 
     return(
         <table className="min-w-full leading-normal">
@@ -11,7 +16,10 @@ export default function UsersTable () {
             <TableHead />
             
             <tbody>
-                <UsersTableRow />
+                {users.map(user => (
+                        <UsersTableRow {...user} />
+                    )
+                )}
                 <Pagination />
             </tbody>
         </table>
