00b0a72cc1987b684617a582ccfe5aaaa19567ee
MahmudK <mahmudkenway@gmail.com>
2022-07-19 16:42:37 +0430

    Create users page

---
+++ pages/adminPanel/users/index.js
@@ -0,0 +1,12 @@
+import Users from "../../../components/adminPanel/users";
+import Layout from "../../../components/layout/panel";
+
+export default function users() {
+    return(
+        <>
+            <Layout>
+                <Users />
+            </Layout>
+        </>
+    )
+}
