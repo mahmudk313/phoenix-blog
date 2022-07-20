2df34386562eb73643c8d4ee671e3ca07b01ab47
MahmudK <mahmudkenway@gmail.com>
2022-07-20 20:22:00 +0430

    Update modal component

--- components/general/modal/index.js
+++ components/general/modal/index.js
@@ -52,10 +52,10 @@ export default function Modal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg pt-6 px-6 font-medium leading-6 text-gray-900"
                   >
                     Add a new user
                   </Dialog.Title>
--- components/general/modal/modalForm/index.js
+++ components/general/modal/modalForm/index.js
@@ -6,7 +6,7 @@ import ModalFormBtns from "./modalFormBtns"
 export default function ModalForm({closeModal}) {
     const fields = [
         {text:"First name", htmlFor:"firstName", type:"text", name:"firstName", id:"firstName", autoComplete: "given-name"},
-        {text:"Last name", htmlFor:"lsatName", type:"text", name:"lastName", id:"lastName", autoComplete: "family-name"},
+        {text:"Last name", htmlFor:"lastName", type:"text", name:"lastName", id:"lastName", autoComplete: "family-name"},
         {text:"Email address", htmlFor:"emailAddress", type:"email", name:"emailAddress", id:"emailAddress", autoComplete: "email"},
         {text:"Password", htmlFor:"password", type:"password", name:"password", id:"password"},
         {text:"Confirm Password", htmlFor:"confirmPassword", type:"password", name:"confirmPassword", id:"confirmPassword"},
@@ -14,7 +14,6 @@ export default function ModalForm({closeModal}) {
     return(
         <div className="mt-5 md:mt-0">
             <form action="#" method="POST">
-                <div className="shadow overflow-hidden sm:rounded-md">
                     <div className="px-4 py-5 bg-white sm:p-6">
                         <div className="grid grid-cols-6 gap-6">
                             {
@@ -23,7 +22,6 @@ export default function ModalForm({closeModal}) {
                         </div>
                     </div>
                     <ModalFormBtns closeModal={closeModal} />
-                </div>
             </form>
         </div>
     )
