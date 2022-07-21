45c0e3e11b311c75ed84fabcd946730b24d2e7e5
MahmudK <mahmudkenway@gmail.com>
2022-07-21 12:17:36 +0430

    update modal style

--- components/general/modal/index.js
+++ components/general/modal/index.js
@@ -17,11 +17,11 @@ export default function Modal() {
 
   return (
     <>
-      <div className="flex items-center justify-start">
+      <div className="flex items-center">
         <button
           type="button"
           onClick={openModal}
-          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          className="rounded-md shadow-sm bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
           Add
         </button>
