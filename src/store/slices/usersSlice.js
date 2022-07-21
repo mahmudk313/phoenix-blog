52926c8e7297ef8a057691fa83d38d4fd54b9a2c
MahmudK <mahmudkenway@gmail.com>
2022-07-21 13:37:51 +0430

    Update initial userstate slice

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -10,8 +10,9 @@ const usersSlice = createSlice({
                 permission: "admin",
                 createDate: "7/19/2022",
                 status: "active",
+                id: 1,
                 email: "mahmudkenway@gmail.com",
-                image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                imageUrl: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             }
         ]
     },
